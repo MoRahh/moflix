7741987cdb40b3e9f6df42e0d4e9abf3c0067846
MoRahh <moodyaburahma@gmail.com>
2024-01-09 15:08:11 +0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAW66oBbq2DqCJ7IKkNvyYu4e8t4OTQ0MU",
+    authDomain: "moflix-cdb9e.firebaseapp.com",
+    projectId: "moflix-cdb9e",
+    storageBucket: "moflix-cdb9e.appspot.com",
+    messagingSenderId: "168793436132",
+    appId: "1:168793436132:web:fd2d8bd49467eabda1e1f1"
+};
